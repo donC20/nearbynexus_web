a64c6b63c1a8affcb3387ff1f1881591e04ab5ae
Don Benny <donbenny916@gmail.com>
2024-03-11 19:08:13 +0530

    responsive

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -25,8 +25,53 @@ const Sidebar = () => {
 
     return (
         <div>
-            <div className={style.sidebar}>
-
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Nearby</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <div className={`${style.sidebar}`}>
+                        <div className="p-3 d-flex flex-column">
+                            <Link to="/dashboard">
+                                <div className={style.sideBarButton}>
+                                    <i className="bi bi-speedometer2 me-3"></i>
+                                    <span>Dashboard</span>
+                                </div>
+                            </Link>
+                            <Link to="/users">
+                                <div className={style.sideBarButton}>
+                                    <i className="bi bi-person me-3"></i>
+                                    <span>Users</span>
+                                </div>
+                            </Link>
+                            <Link to="/jobs_posts">
+                                <div className={style.sideBarButton}>
+                                    <i className="bi bi-person me-3"></i>
+                                    <span>Job posts</span>
+                                </div>
+                            </Link>
+                            <Link to="/services">
+                                <div className={style.sideBarButton}>
+                                    <i className="bi bi-person me-3"></i>
+                                    <span>Services</span>
+                                </div>
+                            </Link>
+                            <Link to="/payments">
+                                <div className={style.sideBarButton}>
+                                    <i className="bi bi-person me-3"></i>
+                                    <span>Payments</span>
+                                </div>
+                            </Link>
+                            <div className={style.sideBarButton} onClick={handleLogout}>
+                                <i className="bi bi-box-arrow-right me-3 text-danger"></i>
+                                <span className='text-danger'>Logout</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={`d-none d-lg-block ${style.sidebar}`}>
                 <div className="p-3 d-flex flex-column">
                     <Link to="/dashboard">
                         <div className={style.sideBarButton}>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -40,18 +40,22 @@ const Navbar = () => {
 
     return (
         <div>
+
+
             <nav className="navContainer position-relative navbar navbar-expand-lg fixed-top bg-body-tertiary">
                 <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
                     <div className={`d-flex flex-row gap-1 justify-content-start align-items-center brandName`}>
                         <img src='/images/logo.png' alt="logo" />
                         <span>NearbyNexus</span>
                     </div>
+
                     {/* <form className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                     </form> */}
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
