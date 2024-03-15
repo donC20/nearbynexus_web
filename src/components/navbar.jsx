fe64d6b4c15beb0d166d4eece2c56c1f0969610a
Don Benny <donbenny916@gmail.com>
2024-03-15 16:22:16 +0530

    notifications partial

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,8 @@ import { doc, getDoc, getFirestore } from 'firebase/firestore';
 import { auth } from '../firebase';
 import Sidebar from './Sidebar';
 import { useNavigate } from 'react-router-dom';
-import { getAllDataOnCondition } from './Apifunction';
+import { fetchDocData, getAllDataOnCondition } from './Apifunction';
+import CommonLoader from './commonLoader';
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -15,6 +16,9 @@ const Navbar = () => {
     const [userOnlineCount, setUserOnlineCount] = useState(0);
     const [generalUserCount, setgeneralUserCount] = useState(0);
     const [hoverIOn, setHoverIOn] = useState('');
+    const [notifications, setnotifications] = useState([]);
+    const [notificationLoader, setnotificationLoader] = useState(true);
+
 
     useEffect(() => {
         // Fetch user data and counts
@@ -35,6 +39,8 @@ const Navbar = () => {
                 }
             }
         };
+
+
 
         // Fetch user counts
         getAllDataOnCondition('users', [
@@ -59,10 +65,48 @@ const Navbar = () => {
         });
 
         fetchData();
+        // fetchNewUsersAndJobs().then(notifications => {
+        //     setnotifications(notifications);
+        // });
     }, [db]);
 
     const userName = userData ? userData.name : "user";
     const userImage = userData ? (userData.image || "raster/avatar-1.png") : "raster/avatar-1.png";
+    console.log(notifications);
+
+    // load notifications
+    const fetchNewUsersAndJobs = async () => {
+        setHoverIOn('notifications');
+        const app_config = await fetchDocData('app_config', 'notifications');
+        const new_user_list = app_config.new_users;
+        const new_jobs_list = app_config.new_jobs;
+
+        const newUsers = await Promise.all(new_user_list.map(async (item) => {
+            const userData = await fetchDocData('users', item);
+            return {
+                type: 'user',
+                data: userData,
+            };
+        }));
+
+        const newJobs = await Promise.all(new_jobs_list.map(async (item) => {
+            const jobData = await fetchDocData('job_posts', item);
+            const postedBy = await fetchDocData('users', jobData.jobPostedBy.id);
+            return {
+                type: 'job',
+                data: jobData,
+                postBy: postedBy
+            };
+        }));
+
+        // Combine new users and job posts into a single array
+        const notifications = [...newUsers, ...newJobs];
+
+        // Now, notifications is an array of objects with type and data
+        setnotificationLoader(false);
+        setnotifications(notifications);
+        return notifications;
+    };
 
     return (
         <div>
@@ -179,73 +223,106 @@ const Navbar = () => {
                         <div className="rigthNavSide d-flex flex-lg-row gap-4">
                             <div className="navRightIcons d-flex flex-lg-row gap-4 align-items-center">
                                 {/* Icon containers */}
-                                <div className={`hoverContainerWrapped position-relative `} onMouseEnter={() => setHoverIOn('people')} onMouseLeave={() => setHoverIOn('')}>
-                                    <div className="d-flex gap-3">
-                                        <div className={`hoverContainerWrapped position-relative `} onMouseEnter={() => setHoverIOn('people')} onMouseLeave={() => setHoverIOn('')}>
-                                            <div className="d-flex gap-2">
-                                                <i className="bi bi-people-fill text-light"></i>
-                                                <span className='text-light'>{userCount}</span>
-                                            </div>
-                                            <div className={`container p-2 bg-white position-absolute rounded shadow hoverActiveContainer ${hoverIOn == "people" ? 'hovered' : ''}`}>
-                                                <small><b>Total users</b></small>
-                                                <hr className='mt-1 mb-1' />
-                                                <small className='text-secondary'>
-                                                    <i className="bi bi-info-circle me-1 text-secondary"></i>
-                                                    Displays the total user count.
-                                                </small>
-                                            </div>
+                                <div className="d-flex gap-3">
+                                    <div className={`hoverContainerWrapped position-relative `} onMouseEnter={() => setHoverIOn('people')} onMouseLeave={() => setHoverIOn('')}>
+                                        <div className="d-flex gap-2">
+                                            <i className="bi bi-people-fill text-light"></i>
+                                            <span className='text-light'>{userCount}</span>
                                         </div>
-                                        {/*  */}
-                                        <div className={`hoverContainerWrapped position-relative `} onMouseEnter={() => setHoverIOn('broadcast')} onMouseLeave={() => setHoverIOn('')}>
-                                            <div className="d-flex gap-2">
-                                                <i className="bi bi-broadcast-pin text-light"></i>
-                                                <span className='text-light'>{userOnlineCount}</span>
-                                            </div>
-                                            <div className={`container p-2 bg-white position-absolute rounded shadow hoverActiveContainer ${hoverIOn == "broadcast" ? 'hovered' : ''}`}>
-                                                <small><b>Online Users</b></small>
-                                                <hr className='mt-1 mb-1' />
-                                                <small className='text-secondary'>
-                                                    <i className="bi bi-info-circle me-1 text-secondary"></i>
-                                                    Displays the total online user count.
-                                                </small>
-                                            </div>
+                                        <div className={`container p-2 bg-white position-absolute rounded shadow hoverActiveContainer ${hoverIOn == "people" ? 'hovered' : ''}`}>
+                                            <small><b>Total users</b></small>
+                                            <hr className='mt-1 mb-1' />
+                                            <small className='text-secondary'>
+                                                <i className="bi bi-info-circle me-1 text-secondary"></i>
+                                                Displays the total user count.
+                                            </small>
                                         </div>
+                                    </div>
+                                    {/*  */}
+                                    <div className={`hoverContainerWrapped position-relative `} onMouseEnter={() => setHoverIOn('broadcast')} onMouseLeave={() => setHoverIOn('')}>
+                                        <div className="d-flex gap-2">
+                                            <i className="bi bi-broadcast-pin text-light"></i>
+                                            <span className='text-light'>{userOnlineCount}</span>
+                                        </div>
+                                        <div className={`container p-2 bg-white position-absolute rounded shadow hoverActiveContainer ${hoverIOn == "broadcast" ? 'hovered' : ''}`}>
+                                            <small><b>Online Users</b></small>
+                                            <hr className='mt-1 mb-1' />
+                                            <small className='text-secondary'>
+                                                <i className="bi bi-info-circle me-1 text-secondary"></i>
+                                                Displays the total online user count.
+                                            </small>
+                                        </div>
+                                    </div>
 
-                                        {/*  */}
-                                        <div className={`hoverContainerWrapped position-relative `} onMouseEnter={() => setHoverIOn('backpack')} onMouseLeave={() => setHoverIOn('')}>
-                                            <div className="d-flex gap-2">
-                                                <i className="bi bi-backpack2-fill text-light"></i>
-                                                <span className='text-light'>{generalUserCount}</span>
-                                            </div>
-                                            <div className={`container p-2 bg-white position-absolute rounded shadow hoverActiveContainer ${hoverIOn == "backpack" ? 'hovered' : ''}`}>
-                                                <small><b>General Users</b></small>
-                                                <hr className='mt-1 mb-1' />
-                                                <small className='text-secondary'>
-                                                    <i className="bi bi-info-circle me-1 text-secondary"></i>
-                                                    Displays the total count of users who are general to the application.
-                                                </small>
-                                            </div>
+                                    {/*  */}
+                                    <div className={`hoverContainerWrapped position-relative `} onMouseEnter={() => setHoverIOn('backpack')} onMouseLeave={() => setHoverIOn('')}>
+                                        <div className="d-flex gap-2">
+                                            <i className="bi bi-backpack2-fill text-light"></i>
+                                            <span className='text-light'>{generalUserCount}</span>
                                         </div>
+                                        <div className={`container p-2 bg-white position-absolute rounded shadow hoverActiveContainer ${hoverIOn == "backpack" ? 'hovered' : ''}`}>
+                                            <small><b>General Users</b></small>
+                                            <hr className='mt-1 mb-1' />
+                                            <small className='text-secondary'>
+                                                <i className="bi bi-info-circle me-1 text-secondary"></i>
+                                                Displays the total count of users who are general to the application.
+                                            </small>
+                                        </div>
+                                    </div>
 
-                                        {/*  */}
-                                        <div className={`hoverContainerWrapped position-relative `} onMouseEnter={() => setHoverIOn('headset')} onMouseLeave={() => setHoverIOn('')}>
-                                            <div className="d-flex gap-2">
-                                                <i className="bi bi-headset text-light"></i>
-                                                <span className='text-light'>{userCount - generalUserCount}</span>
-                                            </div>
-                                            <div className={`container p-2 bg-white position-absolute rounded shadow hoverActiveContainer ${hoverIOn == "headset" ? 'hovered' : ''}`}>
-                                                <small><b>Service providers</b></small>
-                                                <hr className='mt-1 mb-1' />
-                                                <small className='text-secondary'>
-                                                    <i className="bi bi-info-circle me-1 text-secondary"></i>
-                                                    Displays the total count of users who are provides services to the users.
-                                                </small>
-                                            </div>
+                                    {/*  */}
+                                    <div className={`hoverContainerWrapped position-relative `} onMouseEnter={() => setHoverIOn('headset')} onMouseLeave={() => setHoverIOn('')}>
+                                        <div className="d-flex gap-2">
+                                            <i className="bi bi-headset text-light"></i>
+                                            <span className='text-light'>{userCount - generalUserCount}</span>
                                         </div>
-                                        {/*  */}
+                                        <div className={`container p-2 bg-white position-absolute rounded shadow hoverActiveContainer ${hoverIOn == "headset" ? 'hovered' : ''}`}>
+                                            <small><b>Service providers</b></small>
+                                            <hr className='mt-1 mb-1' />
+                                            <small className='text-secondary'>
+                                                <i className="bi bi-info-circle me-1 text-secondary"></i>
+                                                Displays the total count of users who are provides services to the users.
+                                            </small>
+                                        </div>
+                                    </div>
+                                    {/*  */}
+                                    <div className={`notificationsWrapper position-relative `} onMouseEnter={() => fetchNewUsersAndJobs()} onMouseLeave={() => setHoverIOn('')}>
                                         <div className="d-flex gap-2">
                                             <i className="bi bi-bell-fill text-light"></i>
+                                        </div>
+                                        <div className={`container p-2 bg-white position-absolute rounded shadow notificationsWrapperContainer ${hoverIOn == "notifications" ? 'hovered' : ''}`}>
+                                            <small><b>Notifications</b></small>
+                                            <hr className='mt-1 mb-1' />
+                                            {notificationLoader ?
+                                                <div className='d-flex justify-content-center align-items-center'>
+                                                    <CommonLoader />
+                                                </div> :
+                                                <div className="d-flex flex-column">
+                                                    <div>
 
+                                                        {notifications.length > 0 ? notifications.map((item, index) => {
+                                                            if (item.type === 'user') {
+                                                                return (
+                                                                    <div className="d-flex p-2 gap-2" key={index}>
+                                                                        <img src={item.data.image} className='rounded-circle' alt="" />
+                                                                        <small><b>{item.data.name} </b> joined the platform</small>
+                                                                    </div>
+
+                                                                );
+                                                            } else if (item.type === 'job') {
+                                                                return (
+
+                                                                    <div className="d-flex p-2 gap-2" key={index}>
+                                                                        <small><b>{item.postBy.name}</b> posted new <b>{item.data.jobTitle}</b> job</small>
+
+                                                                    </div>
+
+                                                                );
+                                                            }
+                                                            return null; // This should not happen, but it's a good practice to return null for unrecognized types
+                                                        }) : <div><small className='d-flex justify-content-center align-items-center'>No new notifications!</small></div>}
+                                                    </div>
+                                                </div>}
                                         </div>
                                     </div>
                                 </div>
